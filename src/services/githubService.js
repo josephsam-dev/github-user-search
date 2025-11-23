0f5279e5344c408914edadcd59f638400d2577a5
Samuel <josephsegzy@gmail.com>
2025-11-23 17:59:51 +0100

    feat: add GitHub API key support and dynamic user search

--- src/services/githubService.js
+++ src/services/githubService.js
@@ -2,9 +2,15 @@ import axios from "axios";
 
 const BASE_URL = "https://api.github.com";
 
+const config = {
+  headers: {
+    Authorization: `token ${import.meta.env.VITE_APP_GITHUB_API_KEY}`,
+  },
+};
+
 export const searchUser = async (username) => {
   try {
-    const response = await axios.get(`${BASE_URL}/users/${username}`);
+    const response = await axios.get(`${BASE_URL}/users/${username}`, config);
     return response.data;
   } catch (error) {
     console.error("Error fetching GitHub user:", error);
